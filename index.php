f768aae9e666d0da66f549c3a0bc2ab3a3aeb31e
William Myllenberg <wm222au@student.lnu.se>
2018-09-11 09:53:21 +0200

    'Added-translation-templating'

--- index.php
+++ index.php
@@ -1,8 +1,14 @@
 <?php
+  $translation = (isset($_GET['translation']) ? $_GET['translation'] : null); 
   $translate = (isset($_GET['translate']) ? $_GET['translate'] : ''); 
 ?>
 
 <form action="./" method="get">
-  <input type="text" value="<?php echo $translate ?>" name="translate" id="translate">
+  <input type="textarea" value="<?php echo $translate ?>" name="translate" id="translate">
   <input type="submit" value="Translate">
 </form>
+
+<?php if($translation): ?>
+  <h2>Your translation: </h2>
+  <p><?php echo $translation ?></p>
+<?php endif; ?>
